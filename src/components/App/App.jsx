0a96d829978779a06cd509a48c9d8eb9a21fff71
Ricardo <rroque98@gmail.com>
2019-08-09 14:57:11 -0700

    Implement state logic for Search and SubmitButton components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,13 +3,18 @@ import axios from "axios";
 import Search from "../Search";
 import CurrentWeather from "../CurrentWeather";
 import ForecastList from "../ForecastList";
+import SubmitButton from "../SubmitButton";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      location: "oakland"
+      location: "oakland",
+      searchValue: ""
     };
+    this.getWeatherData = this.getWeatherData.bind(this);
+    this.updateSearchValue = this.updateSearchValue.bind(this);
+    this.handleSearchClick = this.handleSearchClick.bind(this);
   }
 
   componentDidMount() {
@@ -41,11 +46,27 @@ class App extends Component {
       });
   }
 
+  updateSearchValue(e) {
+    this.setState({
+      searchValue: e.target.value
+    });
+  }
+
+  handleSearchClick(e) {
+    e.preventDefault();
+    this.setState({ location: this.state.searchValue }, this.getWeatherData);
+  }
+
   render() {
     return (
       <div className="appContainer">
         <header>Trusty Weather</header>
-        <Search />
+        <Search
+          updateSearchValue={this.updateSearchValue}
+          searchValue={this.state.searchValue}
+          placholder="Search by city name"
+        />
+        <SubmitButton value="Search" handleClick={this.handleSearchClick} />
         <CurrentWeather currWeather={this.state.currWeather} />
         <ForecastList fiveDayForecast={this.state.fiveDayForecast} />
       </div>
