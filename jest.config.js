ed37576e3075a6fb77c1bf5c38896ee802ffb3a7
Ricardo <rroque98@gmail.com>
2019-08-09 11:27:15 -0700

    Configure jest for testing

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+    "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js"
+  }
+};
