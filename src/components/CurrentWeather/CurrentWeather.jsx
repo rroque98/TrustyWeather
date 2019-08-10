7f912e12cbf6b9f80b93533ac9bee01ee1430b5d
Ricardo <rroque98@gmail.com>
2019-08-09 20:17:06 -0700

    Add basic styling to CurrentWeather comp

--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./CurrentWeather.css";
 
 const CurrentWeather = ({
   currWeather,
@@ -6,32 +7,45 @@ const CurrentWeather = ({
   determineDayOfWeek
 }) => {
   return (
-    <article className="currWeatherContainer">
+    <React.Fragment>
       {currWeather ? (
-        <section>
-          <img
-            src={`https://www.metaweather.com/static/img/weather/ico/${currWeather.weather_state_abbr}.ico`}
-          />
-          <div>
-            {Math.round(convertCelsiusToFaren(+currWeather.the_temp)) + "\xB0F"}
-          </div>
-          <div>{determineDayOfWeek(currWeather.applicable_date)}</div>
-          <div>{currWeather.weather_state_name}</div>
-          <div>{`High: ${Math.round(
-            convertCelsiusToFaren(+currWeather.max_temp)
-          )}\xB0F`}</div>
-          <div>{`Low: ${Math.round(
-            convertCelsiusToFaren(+currWeather.min_temp)
-          )}\xB0F`}</div>
-          <div>{`${currWeather.wind_direction_compass} ${Math.round(
-            currWeather.wind_speed
-          )} MPH`}</div>
-          <div>{`Air pressure:${currWeather.air_pressure} in`}</div>
-          <div>{currWeather.humidity}%</div>
-          <div>{`Visibility: ${Math.round(currWeather.visibility)} mi`}</div>
-        </section>
+        <article className="currWeatherContainer">
+          <section className="mainLeft">
+            <div className="todayDay">
+              {determineDayOfWeek(currWeather.applicable_date)}
+            </div>
+            <div className="todayTemp">
+              {Math.round(convertCelsiusToFaren(+currWeather.the_temp)) +
+                "\xB0F"}
+            </div>
+          </section>
+
+          <section className="mainMiddle">
+            <img
+              src={`https://www.metaweather.com/static/img/weather/ico/${currWeather.weather_state_abbr}.ico`}
+            />
+            <div>{currWeather.weather_state_name}</div>
+          </section>
+
+          <section className="mainRight">
+            <div>{`High: ${Math.round(
+              convertCelsiusToFaren(+currWeather.max_temp)
+            )}\xB0F`}</div>
+            <div>{`Low: ${Math.round(
+              convertCelsiusToFaren(+currWeather.min_temp)
+            )}\xB0F`}</div>
+            <div>{`${currWeather.wind_direction_compass} ${Math.round(
+              currWeather.wind_speed
+            )} MPH`}</div>
+            <div>{`Air pressure:${Math.round(
+              currWeather.air_pressure
+            )} in`}</div>
+            <div>{currWeather.humidity}%</div>
+            <div>{`Visibility: ${Math.round(currWeather.visibility)} mi`}</div>
+          </section>
+        </article>
       ) : null}
-    </article>
+    </React.Fragment>
   );
 };
 
